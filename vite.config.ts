21a2ab4bdd2c5f556d9b8861864a37d253503904
darlith1610 <thuylinhh1610@gmail.com>
2024-09-19 22:30:56 +0700

    update home page bakery

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -9,6 +11,9 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
   ],
   resolve: {
     alias: {
