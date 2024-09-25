6e73910524352e3fd6b8185b9885c965f17117a8
darlith1610 <thuylinhh1610@gmail.com>
2024-09-25 22:38:33 +0700

    add router for screen

--- components.d.ts
+++ components.d.ts
@@ -7,10 +7,12 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    BCarousel: typeof import('bootstrap-vue-next/components/BCarousel')['BCarousel']
-    BCarouselSlide: typeof import('bootstrap-vue-next/components/BCarousel')['BCarouselSlide']
     Contact: typeof import('./src/components/Contact/Contact.vue')['default']
     ContactComplete: typeof import('./src/components/ContactComplete/ContactComplete.vue')['default']
+    copy: typeof import('./src/components/SearchResults/SearchResults copy.vue')['default']
+    CustomerAccount: typeof import('./src/components/CustomerAccount/CustomerAccount.vue')['default']
+    Customers: typeof import('./src/components/Customers/Customers.vue')['default']
+    EditCustomer: typeof import('./src/components/ProductManagement/EditCustomer.vue')['default']
     Home: typeof import('./src/components/Home/Home.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
@@ -18,10 +20,18 @@ declare module 'vue' {
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     Login: typeof import('./src/components/Login/Login.vue')['default']
+    OrderDetails: typeof import('./src/components/OrderDetails/OrderDetails.vue')['default']
+    OrderManagement: typeof import('./src/components/OrderManagement/OrderManagement.vue')['default']
     Orders: typeof import('./src/components/Orders/Orders.vue')['default']
+    PaymentConfirmation: typeof import('./src/components/PaymentConfirmation/PaymentConfirmation.vue')['default']
+    ProductManagement: typeof import('./src/components/ProductManagement/ProductManagement.vue')['default']
     Products: typeof import('./src/components/Products/Products.vue')['default']
     Register: typeof import('./src/components/Register/Register.vue')['default']
+    ResetPassword: typeof import('./src/components/ResetPassword/ResetPassword.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SearchOrders: typeof import('./src/components/SearchOrders/SearchOrders.vue')['default']
+    SearchResults: typeof import('./src/components/SearchResults/SearchResults.vue')['default']
+    ShoppingCart: typeof import('./src/components/ShoppingCart/ShoppingCart.vue')['default']
   }
 }
