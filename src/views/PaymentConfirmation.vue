453f6717e1d66a134aab2c2e4dfd82d8b3729a2a
darlith1610 <thuylinhh1610@gmail.com>
2024-09-25 22:29:59 +0700

    create payment confirmation screen

---
+++ src/views/PaymentConfirmation.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import PaymentConfirmation from '../components/PaymentConfirmation/PaymentConfirmation.vue';
+</script>
+
+<template>
+  <main>
+    <PaymentConfirmation />
+  </main>
+</template>
